081d6f9adc18699a3d9732aff21638b1bd2a9c92
Jehp00 <Jehp01104006@gmail.com>
2022-12-06 10:46:08 -0500

    Task 1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
+"""
+Module app route Flask
+"""
 
 from flask import Flask
 
 
+"""declaring the app with the Flask class"""
 app=Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
+    """index to web to print a messege running the host and port
+    on the web address (URL)"""
     return "Hello HBNB!"
 
 if __name__=='__main__':
+    """running the app"""
     app.run(host='0.0.0.0', port=5000, debug=True)
