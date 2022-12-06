c2807b2f7537ca193bb127ba18a1192eae391c25
Jehp00 <Jehp01104006@gmail.com>
2022-12-06 11:25:03 -0500

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module app route Flask
+"""
+
+from flask import Flask
+
+
+"""declaring the app with the Flask class"""
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """index to web to print a messege running the host and port
+    on the web address (URL)"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """index to web to print a messege running the host and port
+    on the web second address (URL)"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    new_text = text.replace('_', ' ')
+    return f"C {new_text}"
+
+if __name__ == '__main__':
+    """running the app"""
+    app.run(host='0.0.0.0', port=5000, debug=True)
