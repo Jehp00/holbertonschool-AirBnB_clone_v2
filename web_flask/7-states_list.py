779d8c330d5fabd2c638741054b140cf1a573210
Jehp00 <Jehp01104006@gmail.com>
2022-12-06 16:02:19 -0500

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module list states on index
+"""
+
+from flask import Flask, render_tamplate
+from models import storage
+
+
+app = Falsk(__name__)
+
+@app.route('/states_list')
+def states_list():
+    """list states in html file"""
+    new_states = storage.all('State')
+    return render_tamplate('7-states_list.html', new_states=new_states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """close the objs"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    """run the app flask"""
+    app.run(debug=True, host='0.0.0.0', port=5000)
