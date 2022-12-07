0aaf20102a0f073076aa0d9773cc072bb4d71682
Jehp00 <Jehp01104006@gmail.com>
2022-12-07 16:22:22 -0500

    task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,22 +9,22 @@ from os import getenv
 from sqlalchemy.orm import relationship
 """object flask"""
 app = Flask(_name_)
-app.url_map.strict_slashes = False
+
 
 @app.teardown_appcontext
 def teardown(exc):
     """close the session"""
     storage.close()
 
-@app.route('/cities_by_states')
+
+@app.route('/cities_by_states', strict_slashes = False)
 def cities_by_states():
-    """display html page
+    """
        fetch sorted states to insert into html in UL tag
-       fetch sorted cities in each state into LI tag ->in HTML file
     """
     cities_and_states = [sc for sc in storage.all("State").values()]
     return render_template('8-cities_by_states.html',
-                           state_objs=state_objs)
+                           cities_and_states=cities_and_states)
 
 
 if _name_ == '_main_':
