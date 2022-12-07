d5a88da24187f6da091b3c41d799fb863fa4faa9
Jehp00 <Jehp01104006@gmail.com>
2022-12-07 08:50:38 -0500

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,10 +9,6 @@ from models import storage
 
 """object flask"""
 app = Flask(__name__)
-@app.teardown_appcontext
-def teardown(exc):
-    """close the session"""
-    storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
@@ -20,6 +16,13 @@ def states_list():
     """list states in html file"""
     new_states = storage.all("State")
     return render_tamplate("7-states_list.html", new_states=new_states)
+
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """close the session"""
+    storage.close()
 
 if __name__ == '__main__':
     """run the app flask"""
