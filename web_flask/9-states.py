1c3cc9f5b0fb6d1f154a6403848a8c1617dd9eca
Jehp00 <Jehp01104006@gmail.com>
2022-12-08 10:45:21 -0500

    task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -9,6 +9,12 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
+@app.teardown_appcontext
+def tear_down(exc):
+    """after each request remove current SQLAlchemy session"""
+    storage.close()
+
+
 @app.route("/states", strict_slashes=False)
 def states_list():
     """list states in html file"""
@@ -24,12 +30,6 @@ def states_id(id):
         if state.id == id:
             states = state
     return render_template("9-states.html", states=states)
-
-
-@app.teardown_appcontext
-def tear_down(exc):
-    """after each request remove current SQLAlchemy session"""
-    storage.close()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
